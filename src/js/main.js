ee98086949b655f1f3264a8cb36ba01b1d6342f1
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-10-14 16:15:58 +0200

    zweiten Pfad, Kommentare ergänzt + Korrekturen

--- src/js/main.js
+++ src/js/main.js
@@ -12,22 +12,32 @@ function drawBspl1a(canvas) {
     
 
     // Rechtecke zeichnen
-    context.fillStyle = "hsla(120, 100%, 70%, 1)";
+    context.fillStyle = 'hsla(120, 100%, 70%, 1)';
     context.fillRect(20, 20, 360, 80);
     
-    context.fillStyle = "hsla(220, 100%, 50%, .5)";
+    context.fillStyle = 'hsla(220, 100%, 50%, .5)';
     context.fillRect(10, 10, 180, 180);
 
     
-    // Linie zeichnen (Pfad)
-    context.strokeStyle = "hsla(320, 100%, 25%, 1)";
-    context.lineWidth = 3;
-    context.beginPath();
-    context.moveTo(20, 150);
-    context.lineTo(50, 100);
+    // Linie zeichnen (Pfad) 1
+    context.strokeStyle = 'hsla(320, 100%, 25%, 1)';    // Definiere Linienfarbe
+    context.lineWidth = 3;                              // Definiere Linienbreite
+    context.beginPath();                                // Beginne mit einem Pfad...
+    context.moveTo(20, 150);                            // ... bei den Koordinaten 20, 150 ...
+    context.lineTo(50, 100);                            // ... erstelle eine Linie nach 50, 100 ...
     context.lineTo(80, 150);
     context.lineTo(110, 100);
     context.lineTo(350, 100);
+    context.stroke();                                   // Zeichne den Pfad auf den Canvas
+    context.closePath();                                // Ende der Pfaderstellung
+
+
+    // Linie zeichnen (Pfad) 2
+    context.strokeStyle = 'hsla(20, 100%, 50%, 1)';
+    context.beginPath();
+    context.moveTo(350, 100);
+    context.lineTo(390, 20);
+    context.lineTo(390, 180);
     context.stroke();
     context.closePath();
 }
