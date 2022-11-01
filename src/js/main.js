36f7c772cc173631ef011e4f7bd3086e85e60e4c
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-11-01 10:11:08 +0100

    BsplG: Erleuterungen zu resizeCanvas() hinzugefügt. + Korrektur

--- src/js/main.js
+++ src/js/main.js
@@ -93,14 +93,14 @@ function initContent() {
         },
         
         resizeCanvas() {
-            const currentISmgData = this.ctx.getImageData(0, 0, this.currentCWidth, this.currentCHeight);
+            const currentImgData = this.ctx.getImageData(0, 0, this.currentCWidth, this.currentCHeight);
             this.paintCanvas.width = window.innerWidth * 0.6;
             this.paintCanvas.height = this.paintCanvas.width * 0.66666;
             this.scaleFactors = {
                 x: this.paintCanvas.width / this.currentCWidth,
                 y: this.paintCanvas.height / this.currentCHeight
             }
-            const scaledData = this.getScaledContent(currentISmgData, this.scaleFactors);
+            const scaledData = this.getScaledContent(currentImgData, this.scaleFactors);
             this.ctx.putImageData(scaledData, 0, 0);
             this.setCurrentDimensions({
                 w: this.paintCanvas.width,
